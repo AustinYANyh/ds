94199a2c8825bd5cfa9c5ba80a053adc16435263
AustYANyh <1241528539@qq.com>
2019-05-29 18:13:53 +0800

    新增排序方法的头文件

---
+++ ds-sort-h.h
@@ -0,0 +1,21 @@
+#pragma once
+
+void insertSort(int* array, int size);
+
+void shellSort(int* array, int size);
+
+void selectSort(int* array, int size);
+
+void seclectSort2(int* array, int size);
+
+void HeapSort(int*array, int size);
+
+void BubbleSort(int* array, int size);
+
+void QuickSort(int* array, int left ,int right);
+
+void QucikSortNor(int* array, int size);
+
+void MergeSort(int* array, int size);
+
+void MergeSortNor(int* array, int size);
